d23b5903f827f2eec70113ce279384042aad62fb
aarush123456789 <64641491+aarush123456789@users.noreply.github.com>
2022-06-16 17:35:26 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;		
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball,left,right,groundOBJ,world
+var radius=40
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var ballOption={
+	restitution:0.3,
+	friction:0,
+	density:1.2
+}
+ball=Bodies.circle(260,100,radius/2,ballOption)
+World.add(world,ball)
+
+
+groundOBJ=new Ground(width/2,670,width,20)
+left=new Ground(1100,600,20,120)
+right=new Ground(1350,600,20,120)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ellipse(ball.position.x,ball.position.y,radius,radius)
+groundOBJ.display()
+left.display()
+right.display()
+  drawSprites();
+ 
+}
+
+
+
+function keyPressed() { if (keyCode === UP_ARROW) { Matter.Body.applyForce(ball,ball.position,{x:85,y:-85}); } }
+
+
+
